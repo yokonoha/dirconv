133d507e1e54cdb311696cfa8b25f166596a0cf8
横茶横葉/Y.Yokoha <askjp.team@gmail.com>
2025-02-18 16:47:27 +0900

    Add files via upload

---
+++ dirconv/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dirconv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+         if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+         {
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+         }
+          
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length == 1 && Directory.Exists(files[0]))
+            {
+                textBox1.Text = files[0];
+            }
+            else
+            {
+                MessageBox.Show("単一フォルダのみをドラッグアンドドロップしてください。(複数のフォルダを指定したい場合はその親フォルダを選択してください。) / Please drag and drop only a single folder. (If you want to specify multiple folders, please select their parent folders.)", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length == 1 && Directory.Exists(files[0]))
+            {
+                textBox2.Text = files[0];
+            }
+            else
+            {
+                MessageBox.Show("単一フォルダのみをドラッグアンドドロップしてください。(複数のフォルダを指定したい場合はその親フォルダを選択してください。) / Please drag and drop only a single folder. (If you want to specify multiple folders, please select their parent folders.)", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void textBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void cpyfs2dest(string srcfs,string src,string dest)
+        {
+            string soutaipath = getsoutaipath(src, srcfs);
+            string newdir=Path.Combine(dest, Path.GetDirectoryName(soutaipath));
+            if (!Directory.Exists(newdir))
+            {
+                Directory.CreateDirectory(newdir); //新規作成
+            }
+            string destfs = Path.Combine(newdir, Path.GetFileName(srcfs));//[Copy] destination
+            File.Copy(srcfs, destfs, true); //上書きtrueでcpy!
+        }
+
+        private string getsoutaipath(string basepath,string fullpath)
+        {
+            Uri baseuri = new Uri(basepath.EndsWith(Path.DirectorySeparatorChar.ToString()) ? basepath:basepath+Path.DirectorySeparatorChar);
+            Uri fulluri = new Uri(fullpath);
+            Uri soutaiuri= baseuri.MakeRelativeUri(fulluri); //relativePath(相対パス)を作って格納
+            string soutaipath = Uri.UnescapeDataString(soutaiuri.ToString()); //エスケープさせずにstrに。
+            return soutaipath.Replace('/', Path.DirectorySeparatorChar);//ちゃんとした表現に直して結果を引き渡す。
+        }
+
+        private void cr8bat(string fs,string opfs)
+        {
+            string batpath = Path.Combine(Path.GetTempPath(), "cnv.bat"); //一時ファイルとして作成
+            string content = $"ffmpeg -y -vn -i \"{fs}\" -vn \"{opfs}\""; //ffmpegで変換(アルバムアートつけてやる方法は煩雑なので端折りました ｽﾏﾅｲ...)
+            File.WriteAllText(batpath, content, Encoding.Default); //batファイル作成(UTF-8)
+            var processinfo = new ProcessStartInfo(batpath)
+            {
+                CreateNoWindow = false,
+                UseShellExecute = true
+            };
+            Process.Start(processinfo).WaitForExit(); //変換終了まで待機
+            File.Delete(batpath); //一時ファイル削除
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                string src = textBox1.Text;
+                string dest = textBox2.Text;
+                if (Directory.Exists(src) && Directory.Exists(dest))
+                {
+                    string[] audiofs = Directory.GetFiles(src, "*.*", SearchOption.AllDirectories); //全ファイル収集
+                    foreach (var fs in audiofs)
+                    {
+                        string ext = Path.GetExtension(fs).ToLower();//小文字化して一致させる
+                        if (ext == ".mp3")
+                        {
+                            cpyfs2dest(fs, src, dest);
+                            label12.Text = $"コピーしました:{fs}\nCopied:{fs}";
+                            Application.DoEvents();//GUI更新作業
+                        }
+                        if (ext == ".flac" || ext == ".m4a" || ext == ".wav" || ext == ".opus" || ext==".ac3" ||ext==".mp4")
+                        {
+                            string soutaipath = getsoutaipath(src, fs);
+                            string newdir = Path.Combine(dest, Path.GetDirectoryName(soutaipath));
+                            if (!Directory.Exists(newdir))
+                            {
+                                Directory.CreateDirectory(newdir); //新規作成
+                            }
+                            string opfs = Path.Combine(newdir, Path.GetFileNameWithoutExtension(fs) + ".mp3"); //出力ファイル名
+                            cr8bat(fs, opfs);
+                            label12.Text = $"変換しました:{fs}\nConverted:{fs}";
+                            Application.DoEvents();//GUI更新作業
+                        }
+                    }
+                    label12.Text = "処理が完了しました。\nProcess completed.";
+                }
+                else { label12.Text = "フォルダが存在しません。\nFolder does not exist."; }
+            }
+            else if(radioButton2.Checked)
+            {
+                string src = textBox1.Text;
+                string dest = textBox2.Text;
+                if (Directory.Exists(src) && Directory.Exists(dest))
+                {
+                    string[] audiofs = Directory.GetFiles(src, "*.*", SearchOption.AllDirectories); //全ファイル収集
+                    foreach (var fs in audiofs)
+                    {
+                        string ext = Path.GetExtension(fs).ToLower();//小文字化して一致させる
+                        if (ext == ".m4a")
+                        {
+                            cpyfs2dest(fs, src, dest);
+                            label12.Text = $"コピーしました:{fs}\nCopied:{fs}";
+                            Application.DoEvents();//GUI更新作業
+                        }
+                        if (ext == ".flac" || ext == ".mp3" || ext == ".wav" || ext == ".opus" || ext == ".ac3" || ext == ".mp4")
+                        {
+                            string soutaipath = getsoutaipath(src, fs);
+                            string newdir = Path.Combine(dest, Path.GetDirectoryName(soutaipath));
+                            if (!Directory.Exists(newdir))
+                            {
+                                Directory.CreateDirectory(newdir); //新規作成
+                            }
+                            string opfs = Path.Combine(newdir, Path.GetFileNameWithoutExtension(fs) + ".m4a"); //出力ファイル名
+                            cr8bat(fs, opfs);
+                            label12.Text = $"変換しました:{fs}\nConverted:{fs}";
+                            Application.DoEvents();//GUI更新作業
+                        }
+                    }
+                    label12.Text = "処理が完了しました。\nProcess completed.";
+                }
+                else { label12.Text = "フォルダが存在しません。\nFolder does not exist."; }
+            }
+            else if(radioButton3.Checked)
+            {
+                string src = textBox1.Text;
+                string dest = textBox2.Text;
+                if (Directory.Exists(src) && Directory.Exists(dest))
+                {
+                    string[] audiofs = Directory.GetFiles(src, "*.*", SearchOption.AllDirectories); //全ファイル収集
+                    foreach (var fs in audiofs)
+                    {
+                        string ext = Path.GetExtension(fs).ToLower();//小文字化して一致させる
+                        if (ext == ".flac")
+                        {
+                            cpyfs2dest(fs, src, dest);
+                            label12.Text = $"コピーしました:{fs}\nCopied:{fs}";
+                            Application.DoEvents();//GUI更新作業
+                        }
+                        if (ext == ".mp3" || ext == ".m4a" || ext == ".wav" || ext == ".opus" || ext == ".ac3" || ext == ".mp4")
+                        {
+                            string soutaipath = getsoutaipath(src, fs);
+                            string newdir = Path.Combine(dest, Path.GetDirectoryName(soutaipath));
+                            if (!Directory.Exists(newdir))
+                            {
+                                Directory.CreateDirectory(newdir); //新規作成
+                            }
+                            string opfs = Path.Combine(newdir, Path.GetFileNameWithoutExtension(fs) + ".flac"); //出力ファイル名
+                            cr8bat(fs, opfs);
+                            label12.Text = $"変換しました:{fs}\nConverted:{fs}";
+                            Application.DoEvents();//GUI更新作業
+                        }
+                    }
+                    label12.Text = "処理が完了しました。\nProcess completed.";
+                }
+                else { label12.Text = "フォルダが存在しません。\nFolder does not exist."; }
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Form2を表示
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+    }
+}
---
+++ dirconv/Form2.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace dirconv
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 19F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DirConv";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 12F);
+            this.label2.Location = new System.Drawing.Point(135, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(269, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Developed by Y.Yokoha(横茶横葉)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 12F);
+            this.label3.Location = new System.Drawing.Point(150, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(358, 72);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "昔の携帯ゲーム機とカーナビにて\r\n音楽をフォルダ構造そのままに変換&&コピーして\r\n聴いてみたかったので製作しました!";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(154, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "開発者より";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 321);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(499, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "このソフトウェアは単体では動作しません。配布ページ記載の説明をよくご覧になったうえで実行してください。";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(205, 342);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(380, 12);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "©2025 横茶横葉 All Rights Reserved.    ご利用いただきありがとうございます!!";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(256, 279);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(137, 12);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://yokonoha.github.io";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(90, 279);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(156, 12);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Get more info on my website!";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(90, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 12);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Source codes:";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(172, 257);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(196, 12);
+            this.linkLabel2.TabIndex = 10;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://github.com/yokonoha/dirconv";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(32, 226);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(244, 12);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "何かご不明な点等ございましたらお気軽にSNSまで!";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(282, 226);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(162, 12);
+            this.linkLabel3.TabIndex = 12;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "https://x.com/yokocha_yokoha";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::dirconv.Properties.Resources.trademark;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 363);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "このソフトウェア&製作者について";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+    }
+}
